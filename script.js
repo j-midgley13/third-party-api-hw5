d277e5915313a66ae8b19557cccd2b59f3113575
Jameson Midgley <jameson.midgley@gmail.com>
2020-06-24 21:26:56 -0600

    Got formatting for time blocks.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // start of js and jQuery
+// Just getting formatting for rows
+var currentDay = moment().format("MM/DD/YYYY")
+$("#currentDay").text(currentDay)
+
+var hourRow = $("<div>").addClass("row");
+
+var hourBlock = $("<textarea>").addClass("hour").text("9 am");
+
+var notesBlock = $("<input>").addClass("time-block").attr("placeholder", "Enter Notes");
+
+var saveBtn = $("<button>").addClass("saveBtn").text("save");
+
+hourRow.append(hourBlock).append(notesBlock).append(saveBtn);
+
+var container = $(".container").append(hourRow);
+
